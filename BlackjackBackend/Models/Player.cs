ac4615a0e6f4f1548b6f64228ec35a30d6665bcc
PattyBurke99 <pat.burke1917@gmail.com>
2024-11-19 13:21:04 -0400

    Created Player model as a container to hold player data in memory

---
+++ BlackjackBackend/Models/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace BlackjackBackend.Models
+{
+    public class Player
+    {
+        private string _id;
+        private int _money { get; set; }
+
+        public Player(string id) 
+        { 
+            _id = id;
+            _money = 100;
+        }
+    }
+}
